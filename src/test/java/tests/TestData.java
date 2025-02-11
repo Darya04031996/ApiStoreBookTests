72c92843c090556fe7902b76389b94a88ec57715
Darya Melgunova <darya.melgunova@gmail.com>
2025-02-11 11:53:58 +0300

    added first Tests in main2

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -4,11 +4,13 @@ import models.BookDataModel;
 import models.CredentialsModel;
 
 public class TestData {
-   private static String login = "test123456",
-                         password = "Test123456@";
+    private static final String login = System.getenv("USERNAME");
+    private static final String password = System.getenv("PASSWORD");
+
     public static CredentialsModel credentials = new CredentialsModel(login, password);
-    private static String book_isbn = "9781449325862";
-    private static String book_title = "Git Pocket Guide";
+
+    private static final String book_isbn = "9781449325862";
+    private static final String book_title = "Git Pocket Guide";
     public static final BookDataModel book = new BookDataModel(book_isbn, book_title);
 }
 
