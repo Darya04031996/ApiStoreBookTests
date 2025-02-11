c18ca6a9208c14c394b9295c0d2fd21fbf0f3930
Darya Melgunova <darya.melgunova@gmail.com>
2025-02-11 19:04:39 +0300

    added first Tests in main2

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -4,8 +4,8 @@ import models.BookDataModel;
 import models.CredentialsModel;
 
 public class TestData {
-    private static final String login = System.getProperty("userLogin");
-    private static final String password = System.getProperty("UserPassword");
+    private static final String login = System.getProperty("userName");
+    private static final String password = System.getProperty("password");
 
     public static CredentialsModel credentials = new CredentialsModel(login, password);
 
